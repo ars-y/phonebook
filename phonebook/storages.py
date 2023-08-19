f720d308fe6360645408cc03e9d906193049cb45
ArslanYadov <arsyy90@gmail.com>
2023-08-19 19:27:50 +0300

    added find and update methods for inherit of BasePhoneBook

--- phonebook/storages.py
+++ phonebook/storages.py
@@ -3,41 +3,65 @@ import shelve
 from pathlib import Path
 
 from .constants import CONTACTS_FILE, DB_NAME
-from .models import ContactBaseModel
+from .models import Contact
+from .utils import concat_dict_values
 
 
 class BasePhoneBook:
     """Базовая модель телефонного справочника."""
 
-    def __init__(self, contact: ContactBaseModel | None = None) -> None:
+    def __init__(self, contact: Contact | None = None) -> None:
         self._contact = contact
 
-    def save(self, *, flush: bool = True) -> None:
+    def save(
+        self,
+        filename: str | Path = DB_NAME,
+        *,
+        flush: bool = True
+    ) -> None:
         """
         Сохраняет контакт в базу данных, после
         чего сбрасывает данные в файл,
         если ключевой не имеет значения False.
 
         Аргументы:
-            - **flush**: булевский аргумент,
-            который сбрасывает контакты в файл
-            если значение True (по умолчанию)
+            - **filename**: имя или путь до файла для хранения;
+            - **flush**: ключевой аргумент,
+            флаг для сбрасывания контактов в текстовый файл.
         """
-        with shelve.open(DB_NAME) as db:
+        with shelve.open(filename) as db:
             db[self._contact._id] = self._contact
+
         if flush:
             self.flush()
 
-    def load(self) -> list:
-        """Выгружает список контактов из файла."""
+    def load(
+        self,
+        filename: str | Path = DB_NAME,
+        *,
+        contact_id: str = None,
+        multiple: bool = True
+    ) -> Contact | list:
+        """
+        Выгружает список контактов из файла.
+        Вернет объект контакта, если убран флаг multiple
+        и задан ID контакта, при наличии его в базе данных.
+
+        Аргументы:
+            - **filename**: имя или путь до файла для хранения;
+            - **contact_id**: ключевой аргумент ID контакта;
+            - **multiple**: ключевой аргумент,
+            флаг для выгрузки списка контактов.
+        """
         data: list = []
-        with shelve.open(DB_NAME) as db:
+        with shelve.open(filename) as db:
+            if not multiple:
+                return db.get(contact_id)
+
             for key in db:
                 data.append(db[key])
-        return data
 
-    def find(self, patterns: list[str]) -> ContactBaseModel:
-        """Находит контакт в записной книжке по характеристикам."""
+        return data
 
     def flush(self, filename: str | Path = CONTACTS_FILE) -> None:
         """
@@ -46,7 +70,7 @@ class BasePhoneBook:
         указанный по умолчанию.
 
         Аргументы:
-            - **filename**: путь до файла.
+            - **filename**: имя или путь до файла для хранения;
         """
         contacts: list = self.load()
         with open(filename, 'w', encoding='utf-8') as f:
@@ -60,17 +84,29 @@ class BasePhoneBook:
             writer = csv.DictWriter(f, row.keys())
             writer.writerows(rows)
 
-    def remove(self, *, flush: bool = True) -> None:
-        """Удаляет запись из базы данных."""
-        with shelve.open(DB_NAME) as db:
-            key = self._contact._id
+    def remove(
+        self,
+        filename: str | Path = DB_NAME,
+        *,
+        contact_id: str = None,
+        flush: bool = True
+    ) -> None:
+        """
+        Удаляет запись из базы данных.
+
+        Аргументы:
+            - **filename**: имя или путь до файла для хранения;
+            - **contact_id**: ключевой аргумент ID контакта;
+            - **flush**: ключевой аргумент,
+            флаг для сбрасывания контактов в текстовый файл.
+        """
+        with shelve.open(filename) as db:
+            key = self._contact._id if not contact_id else contact_id
             if key in db:
                 del db[key]
+
         if flush:
             self.flush()
-
-    def update(self):
-        """Обновляет данные контакта."""
 
     def __str__(self) -> str:
         pass
@@ -78,4 +114,25 @@ class BasePhoneBook:
 
 class PhoneBook(BasePhoneBook):
     """Модель телефонного справочника."""
-    pass
+
+    def __init__(self, contact: Contact | None = None) -> None:
+        super().__init__(contact)
+
+    def find(self, contact_id: str) -> Contact | None:
+        """Возвращает объект контакта пo его ID."""
+        return self.load(contact_id=contact_id, multiple=False)
+
+    def find_all(self, patterns: list[str]) -> list[Contact]:
+        """Возвращает список контактов, по заданным критериям."""
+
+    def update(self) -> None:
+        """Обновляет данные контакта."""
+        old_contact: Contact | None = self.find(self._contact._id)
+        if not old_contact:
+            raise KeyError('данного контакта не существует')
+
+        self.remove(contact_id=old_contact._id, flush=False)
+        self._contact._id: str = concat_dict_values(
+            self._contact.model_dump(exclude_unset=True)
+        )
+        self.save()

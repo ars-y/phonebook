bb762bd858d59d667df7e7e8672b8c72d16c0ed3
ArslanYadov <arsyy90@gmail.com>
2023-08-20 21:40:17 +0300

    added card and short representation for Contact model

--- phonebook/models.py
+++ phonebook/models.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
+from .utils import field_separator
 
 
 class ContactBaseModel(BaseModel):
-    """Базовая модель контакта в телефонном справочнике."""
+    """Base Contact model."""
 
     _id: int | None = None
     first_name: str | None = None
@@ -14,5 +15,41 @@ class ContactBaseModel(BaseModel):
 
 
 class Contact(ContactBaseModel):
-    """Модель контакта."""
-    pass
+    """Contact model."""
+
+    @property
+    def card_view(self) -> str:
+        """Card view of the contact model."""
+        contact_card: list = []
+        sepr: str = field_separator()
+
+        if self.first_name:
+            contact_card.append(f'First name: {self.first_name}{sepr}')
+
+        if self.last_name:
+            contact_card.append(f'Last name: {self.last_name}{sepr}')
+
+        if self.surname:
+            contact_card.append(f'Surname: {self.surname}{sepr}')
+
+        if self.company:
+            contact_card.append(f'Company: {self.company}{sepr}')
+
+        if self.work:
+            contact_card.append(f'Mobile: {self.work}{sepr}')
+
+        if self.mobile:
+            contact_card.append(f'Work: {self.mobile}{sepr}')
+
+        return ''.join(contact_card)
+
+    @property
+    def short_view(self) -> str:
+        """
+        Brief view of the contact model.
+        Return first not None field value.
+        """
+        fields: dict = self.model_dump()
+        for name in fields:
+            if fields[name]:
+                return fields[name]

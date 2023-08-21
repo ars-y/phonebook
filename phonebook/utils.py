4a285fd387113e5ee2e3b19cfb48a143c554cc4f
ArslanYadov <arsyy90@gmail.com>
2023-08-21 22:23:45 +0300

    removed some funcs due to transfer in ioworkers.py

--- phonebook/utils.py
+++ phonebook/utils.py
@@ -1,29 +1,5 @@
-import os
-import sys
-
-
 def concat_dict_values(data: dict) -> str:
     """Concatenation of all dict values in single string."""
     return ''.join(
         str(value).replace(' ', '').lower() for value in data.values()
     )
-
-
-def clear() -> None:
-    """Clearing the terminal."""
-    if os.name == 'nt':
-        _ = os.system('cls')
-    else:
-        _ = os.system('clear')
-
-
-def exit() -> None:
-    """Terminate the program."""
-    clear()
-    sys.exit()
-
-
-def field_separator(sep_char: str = '-', length: int = 30) -> str:
-    """Constructing field separator."""
-    carry: str = '\n'
-    return carry + sep_char*length + carry

d874284d16178434f7e09bb5876a3edd277a48fb
ArslanYadov <arsyy90@gmail.com>
2023-08-21 23:59:35 +0300

    upd handlers

--- phonebook/handlers.py
+++ phonebook/handlers.py
@@ -1,29 +1,98 @@
-def main_menu():
-    """
-    Обработчик главного меню.
-    Отображает отсортированный по имени список из,
-    не более чем, 10 контактов.
-    Список пронумерован от 0 до 9, где номер
-    это выбор определенного контакта.
-    Доступные действия:
-        - пролистать список вперед, назад;
-        - выбрать контакт из списка;
-        - найти контакт;
-        - добавить контакт;
-        - закрыть приложение.
-    """
+from typing import Callable
+
+from .storages import PhoneBook
+from .models import Contact
+from .messages import help_message, render, request_action
+from .ioworkers import console
+from .validators import validate
 
 
-def create_contact():
+PAGE_SIZE: int = 10
+
+
+def main_menu(context: dict | None = None):
+    """
+    Main menu handler.
+    Display a list contacts sorted by name.
+
+    Available:
+        - pagination with 10 contacts per page;
+        - select contact from list;
+        - search contact;
+        - add new contact;
+        - quit application.
+    """
+    console.clear()
+
+    phone_book: PhoneBook = PhoneBook()
+    contacts: list[Contact] = phone_book.load()
+
+    options: dict = {
+        '-a': create_contact,
+        '-f': find_contact,
+        '-q': quit_app,
+        '-h': help_info,
+        '--help': help_info,
+    }
+    contacts_per_page: list = contacts[:PAGE_SIZE]
+    options.update(
+        **dict.fromkeys(
+            (str(i) for i in range(len(contacts_per_page))), set_field
+        )
+    )
+
+    render(main_menu, contacts)
+
+    action: str = request_action(options)
+
+    # TODO: make pagination
+    if action.isdigit() and int(action) in len(contacts_per_page):
+        ...
+
+    console.clear()
+    options[action.lower()]({'handler': main_menu})
+
+
+def create_contact(context: dict | None = None):
     """
     Обработчик создания контакта.
     Доступные действия:
         - добавления указанных полей контакта.
         - отмена операции создания контакта.
     """
+    digits = range(1, 7)
+
+    context['handler'] = create_contact
+    options: dict = {
+        '-s': save_contact,
+        '-c': main_menu,
+        '-h': help_info,
+        '--help': help_info,
+    }
+    options.update(**dict.fromkeys((str(i) for i in digits), set_field))
+
+    contact: Contact = context.get('contact', Contact())
+    render(create_contact, contact)
+    action: str = request_action(options)
+
+    if action.isdigit() and int(action) in digits:
+        fileds: dict = dict(zip(digits, contact.model_fields))
+        field = fileds[int(action)]
+        value = console.read('Enter value: ')
+
+        if field in ('mobile', 'work'):
+            value = validate(field, value)
+
+        context['field'] = field
+        context['value'] = value
+
+    context['contact'] = contact
+
+    console.clear()
+    options[action.lower()](context)
 
 
-def edit_contact():
+def edit_contact(context: dict | None = None):
     """
     Обработчик изменения полей контакта.
     Доступные действия:
@@ -33,7 +102,7 @@ def edit_contact():
     """
 
 
-def find_contact():
+def find_contact(context: dict | None = None):
     """
     Обработчик поиска контакта.
     Доступные действия:
@@ -41,3 +110,42 @@ def find_contact():
         по которым должен произвестись поиск;
         - отмена операции изменения контакта.
     """
+
+
+def save_contact(context: dict | None = None):
+    """Save contact in DB handler."""
+    contact: Contact = context.get('contact')
+    phone_book: PhoneBook = PhoneBook(contact)
+    phone_book.save()
+
+
+def set_field(context: dict | None = None):
+    """Set value in model field."""
+    handler: Callable = context.get('handler')
+    contact: Contact = context.get('contact')
+    field_name: str = context.get('field')
+    field_value: str = context.get('value')
+
+    setattr(contact, field_name, field_value)
+    context['contact'] = contact
+    handler(context)
+
+
+def help_info(context: dict | None = None):
+    """Print help info about menu handler."""
+    handler: Callable = context.get('handler')
+    handler_name: str = handler.__name__ + '_options'
+    console.write(help_message(handler_name))
+    console.read('Press ENTER to continue ')
+    handler(context)
+
+
+def run_app():
+    """Running application in cycle."""
+    while True:
+        main_menu()
+
+
+def quit_app(context: dict | None = None):
+    """Escape from application."""
+    exit()

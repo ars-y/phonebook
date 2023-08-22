3375aad331874fd398f6c396cd74348aff06194f
ArslanYadov <arsyy90@gmail.com>
2023-08-22 23:34:06 +0300

    added func to check elements contains target value

--- phonebook/utils.py
+++ phonebook/utils.py
@@ -3,3 +3,39 @@ def concat_dict_values(data: dict) -> str:
     return ''.join(
         str(value).replace(' ', '').lower() for value in data.values()
     )
+
+
+def contains(
+    elements: list[str],
+    target: str,
+    *,
+    _sort: bool = False,
+    _unique: bool = False
+) -> bool:
+    """Checking that elements contain a target value using binary search."""
+    def binary_search(
+        elements: list,
+        target: str,
+        left: int,
+        right: int
+    ) -> bool:
+        if left > right:
+            return False
+
+        middle: int = (left + right) // 2
+
+        if target in elements[middle]:
+            return True
+
+        if target > elements[middle]:
+            return binary_search(elements, target, middle + 1, right)
+
+        return binary_search(elements, target, left, middle - 1)
+
+    if not _unique:
+        elements: list = [element for element in elements if element]
+
+    if not _sort:
+        elements.sort()
+
+    return binary_search(elements, target, 0, len(elements) - 1)

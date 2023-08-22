fa7e19026f5fb5ce77d40c6c29be40177e9be7bf
ArslanYadov <arsyy90@gmail.com>
2023-08-22 23:28:14 +0300

    added decorator to create a frame for render app menu

---
+++ phonebook/decorators.py
@@ -0,0 +1,19 @@
+from .ioworkers import console
+
+
+def frame(headers: list[str], footers: list[str]):
+    """Decorator to create header and footer frame."""
+    def frame_decorator(func):
+        def wrapper(*args, **kwargs):
+            header: str = '| ' + ' | '.join(headers) + ' |'
+            footer: str = '| ' + ' | '.join(footers) + ' |'
+
+            console.write(header.center(50, '#'))
+            console.write('\n')
+
+            func(*args, **kwargs)
+
+            console.write('\n')
+            console.write(footer.center(50, '#'))
+        return wrapper
+    return frame_decorator

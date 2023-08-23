a4fa62c22342810e751cde72122354819f93a130
ArslanYadov <arsyy90@gmail.com>
2023-08-23 23:03:05 +0300

    added funcs: remove item from list and update list with new value

--- phonebook/utils.py
+++ phonebook/utils.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 def concat_dict_values(data: dict) -> str:
     """Concatenation of all dict values in single string."""
     return ''.join(
@@ -12,9 +15,19 @@ def contains(
     _sort: bool = False,
     _unique: bool = False
 ) -> bool:
-    """Checking that elements contain a target value using binary search."""
+    """
+    Checking that elements contain a target value using binary search.
+
+    Args:
+        - **elements**: list with str objects inside;
+        - **target**: value to be found in str;
+        - **_sort**: key argument - boolean flag,
+        make sorted list if it's False (default);
+        - **_unique**: key argument - boolean flag,
+        remove duplicates from list if it's False (default).
+    """
     def binary_search(
-        elements: list,
+        elements: list[str],
         target: str,
         left: int,
         right: int
@@ -24,10 +37,12 @@ def contains(
 
         middle: int = (left + right) // 2
 
-        if target in elements[middle]:
+        element: str = elements[middle].lower()
+
+        if target in element:
             return True
 
-        if target > elements[middle]:
+        if target > element:
             return binary_search(elements, target, middle + 1, right)
 
         return binary_search(elements, target, left, middle - 1)
@@ -39,3 +54,25 @@ def contains(
         elements.sort()
 
     return binary_search(elements, target, 0, len(elements) - 1)
+
+
+def replace_item(items: list, value: Any) -> list:
+    """
+    Return a new list with item replaced by the new value.
+
+    Args:
+        - **items**: list object;
+        - **value**: any type that can be compared.
+    """
+    return [value if item == value else item for item in items]
+
+
+def remove_item(items: list, value: Any) -> list:
+    """
+    Return a new list without old value.
+
+    Args:
+        - **items**: list object;
+        - **value**: any type that can be compared.
+    """
+    return [item for item in items if item != value]

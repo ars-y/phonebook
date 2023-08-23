a7e41a6bdd5dfc0e6565b3d07953501ebf395118
ArslanYadov <arsyy90@gmail.com>
2023-08-23 23:05:30 +0300

    main module to run the application

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+from os import error
+
+from phonebook.config import argument_parser, FILE_INIT_KEY
+from phonebook.handlers import run_app
+
+
+def main():
+    try:
+        run_app()
+    except error as exc:
+        exc_message: str = str(exc)
+        if exc_message.startswith('[Errno 2]'):
+            exc_info: str = (
+                '\nIf you are running application for the first time, '
+                f'use key [{FILE_INIT_KEY}] '
+                'to create the necessary directories.\n'
+            )
+            exc_message += exc_info
+
+        sys.stdout.write(exc_message)
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) > 1:
+        argument_parser()
+
+    main()
